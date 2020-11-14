db8193a174e0537ca06cbd68759cedd931513e25
andonis1616 <cuzencoandreirobert2008@gmail.com>
2020-11-14 17:05:12 +0200

    docker

---
+++ docker/code/UsoForum/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Home Page</h1>')
+def forum(request):
+    return HttpResponse('<h1>Forum Page</h1>')
