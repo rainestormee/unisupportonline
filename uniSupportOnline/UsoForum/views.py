e72a1f3fdf81a3cf75d3ab235a073458db5472d4
andonis1616 <cuzencoandreirobert2008@gmail.com>
2020-11-15 00:35:01 +0200

    init

--- uniSupportOnline/UsoForum/views.py
+++ uniSupportOnline/UsoForum/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import tuple_factory
+import os
 
+cloud_config={
+    'secure_connect_bundle': os.path.join(os.path.realpath(os.path.dirname(__file__)), 'secure-connect-unisupportdb.zip')
+}
+
+auth_provider=PlainTextAuthProvider('unisupportdb','supersecurepassword')
+cluster=Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session=cluster.connect()
+
+session.row_factory = tuple_factory
 
 def subset_dic(subset, superset):
     return len(set(subset.items()) & set(superset.items())) == len(subset)
@@ -26,15 +36,6 @@ def dashboard(request):
 
 
 def help(request):
-    cloud_config={
-    'secure_connect_bundle': 'V:\Hackathon\Hackathon\secure-connect-unisupportdb.zip'
-    }
-
-    auth_provider=PlainTextAuthProvider('unisupportdb','supersecurepassword')
-    cluster=Cluster(cloud=cloud_config, auth_provider=auth_provider)
-    session=cluster.connect()
-
-    session.row_factory = tuple_factory
     try:
         row = session.execute("SELECT senderid, senderusername, messagecontent from unisupport.messages WHERE receiverid = 2 ALLOW FILTERING;")
     except:
