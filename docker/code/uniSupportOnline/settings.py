154fff4a4792ef9dabed0a255438f8908187adfc
rainestormee <ryanarrowsmith01@gmail.com>
2020-11-14 15:07:39 +0000

    Ryans Automagical Push Tool Because Hes Lazy

--- docker/code/uniSupportOnline/settings.py
+++ docker/code/uniSupportOnline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k$9l4l^2m2xt))*=i==hek^)=6h%pc(lco2i)z^s*1rwdi5a7)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
