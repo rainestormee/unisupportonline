20d3b8281c3f73657b20eea9f0ead36ddbae92af
BuildTools <alex.delaitre123@gmail.com>
2020-11-14 18:56:03 +0000

    html+css

--- uniSupportOnline/UsoForum/urls.py
+++ uniSupportOnline/UsoForum/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home, name='uso-home'),
-    path('forum/',views.forum, name='uso-forums')
+    path('forum/',views.forum, name='uso-forums'),
+    path('about/',views.about, name='uso-about')
 ]
