7610b21d77d706c4495dac50f62d4aa73788bfef
BuildTools <alex.delaitre123@gmail.com>
2020-11-14 22:09:47 +0000

    preview messages, kanye still here

--- uniSupportOnline/UsoForum/views.py
+++ uniSupportOnline/UsoForum/views.py
@@ -4,36 +4,9 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import tuple_factory
 
-cloud_config={
-    'secure_connect_bundle': 'V:\Hackathon\Hackathon\secure-connect-unisupportdb.zip'
-    }
-
-auth_provider=PlainTextAuthProvider('unisupportdb','supersecurepassword')
-cluster=Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session=cluster.connect()
-
-session.row_factory = tuple_factory
-
-row = session.execute("SELECT senderid, senderusername, messagecontent from unisupport.messages WHERE receiverid = 2 ALLOW FILTERING;")
-
-contacts=[]
 
 def subset_dic(subset, superset):
     return len(set(subset.items()) & set(superset.items())) == len(subset)
-
-row=reversed(list(row))
-
-for user_row in row:
-    inContacts=False
-    addRow={'id':user_row[0], 'name':user_row[1], 'content':user_row[2]}
-    for i in range(len(contacts)):
-        if subset_dic({'id':user_row[0], 'name':user_row[1]}, contacts[i]):
-            inContacts=True
-    if inContacts==False:
-        contacts.append(addRow)
-    print(user_row)
-
-
 # Create your views here.
 
 def home(request):
@@ -53,6 +26,33 @@ def dashboard(request):
 
 
 def help(request):
+    cloud_config={
+    'secure_connect_bundle': 'V:\Hackathon\Hackathon\secure-connect-unisupportdb.zip'
+    }
+
+    auth_provider=PlainTextAuthProvider('unisupportdb','supersecurepassword')
+    cluster=Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session=cluster.connect()
+
+    session.row_factory = tuple_factory
+
+    row = session.execute("SELECT senderid, senderusername, messagecontent from unisupport.messages WHERE receiverid = 2 ALLOW FILTERING;")
+
+    contacts=[]
+
+    row=reversed(list(row))
+
+    for user_row in row:
+        inContacts=False
+        addRow={'id':user_row[0], 'name':user_row[1], 'content':user_row[2]}
+        for i in range(len(contacts)):
+            if subset_dic({'id':user_row[0], 'name':user_row[1]}, contacts[i]):
+                inContacts=True
+        if inContacts==False:
+            contacts.append(addRow)
+        print(user_row)
+    print(contacts)
+    #print("dingdong")
     return render(request, 'help.html', {'users':contacts})
 
 
