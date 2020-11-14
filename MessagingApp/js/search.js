ab52080d7d91dcbdc45dd188108641b02e7cf8a5
rainestormee <ryanarrowsmith01@gmail.com>
2020-11-14 17:45:25 +0000

    aa

--- MessagingApp/js/search.js
+++ MessagingApp/js/search.js
@@ -1,20 +1,28 @@
 function showResult(str) {
     if (str.length === 0) {
- //       document.getElementById("conversation-list").style.display = "block";
-        Array.from(document.getElementById("conversation-list").children).forEach(e => e.style.display = "grid");
-
+        this.getConversations().forEach(e => e.style.display = "grid");
     } else {
-        Array.from(document.getElementById("conversation-list").children).forEach(e => {
-            console.log(e.id);
-            if (e.id == null) return;
+        this.getConversations().forEach(e => {
             if (e.id.toLowerCase().includes(str.toLowerCase())) {
                 e.style.display = "grid";
-
             } else {
                 e.style.display = "none";
             }
-
         });
-//        document.getElementById("conversation-list").style.display = "none";
     }
+}
+
+function switchChatMessage(name) {
+    this.getConversations().forEach(e => {
+        if (e.id === name) {
+            e.class = "conversation active";
+            console.log(e.id);
+        } else {
+            e.class = "conversation";
+        }
+    });
+}
+
+function getConversations() {
+    return Array.from(document.getElementById("conversation-list").children).filter(e => (e != null && e.id != null));
 }
