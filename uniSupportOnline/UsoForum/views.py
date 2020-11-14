d8c097177856001636ec95c951514d2775bfb4d5
rainestormee <ryanarrowsmith01@gmail.com>
2020-11-14 20:31:31 +0000

    Ryans Automagical Push Tool Because Hes Lazy

--- uniSupportOnline/UsoForum/views.py
+++ uniSupportOnline/UsoForum/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import sys, os
+
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
-def about(request):
-    return render(request,'about.html')
-def contact(request):
-    return render(request,'contact.html')
-def dashboard(request):
-    return render(request,'dashboard.html')
-def help(request):
-    return render(request,'help.html')
-def login(request):
-    return render(request,'login.html')
-def search(request):
-    return render(request,'search.html')
-def signup(request):
-    return render(request,'signup.html')
-def terms(request):
-    return render(request,'terms.html')
+    return render(request, 'home.html')
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def help(request):
+    return render(request, 'help.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def terms(request):
+    return render(request, 'terms.html')
