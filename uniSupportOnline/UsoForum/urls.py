1cd3c6837bc60be8921668b6e737dee33e414eac
BuildTools <alex.delaitre123@gmail.com>
2020-11-14 20:49:44 +0000

    fixing the issues caused by stinker

--- uniSupportOnline/UsoForum/urls.py
+++ uniSupportOnline/UsoForum/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='uso-home'),
     path('about/', views.about, name='uso-about'),
-    path('contact/', views.contact, name='uso-about'),
-    path('dashboard/', views.dashboard, name='uso-about'),
-    path('help/', views.help, name='uso-about'),
-    path('login/', views.login, name='uso-about'),
-    path('search/', views.search, name='uso-about'),
-    path('signup/', views.signup, name='uso-about'),
-    path('terms/', views.terms, name='uso-about'),
+    path('contact/', views.contact, name='uso-contact'),
+    path('dashboard/', views.dashboard, name='uso-dashboard'),
+    path('help/', views.help, name='uso-help'),
+    path('login/', views.login, name='uso-login'),
+    path('search/', views.search, name='uso-search'),
+    path('signup/', views.signup, name='uso-signup'),
+    path('terms/', views.terms, name='uso-terms'),
 
 ]
