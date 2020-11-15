e387fea64af8039f38925eb1ab45555c7941db4b
BuildTools <alex.delaitre123@gmail.com>
2020-11-15 15:56:50 +0000

    not allowed to enter help without account

--- uniSupportOnline/UsoForum/views.py
+++ uniSupportOnline/UsoForum/views.py
@@ -80,6 +80,8 @@ def help(request):
         username=""
     if len(username)<1:
         isLogged=False
+        render(request, 'home.html' {'users': contacts, 'messages': messages, 'displayUser': displayUser, 'userlogged': userlogged })
+        return render    
     else:
         isLogged=True
     userlogged={'username':username,'bool':isLogged}
