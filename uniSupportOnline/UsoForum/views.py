dd22cef67fb3f7559d847fa5292fb61833d9f359
rainestormee <ryanarrowsmith01@gmail.com>
2020-11-15 13:14:00 +0000

    Ryans Automagical Push Tool Because Hes Lazy

--- uniSupportOnline/UsoForum/views.py
+++ uniSupportOnline/UsoForum/views.py
@@ -150,9 +150,14 @@ def signupCode(request):
         username = username.encode('ascii', 'strict').decode()
         password = validatePassword(password)
         password=hashlib.sha512(password).hexdigest()
-        row = session.execute('SELECT MAX(userid) AS max FROM unisupport.users')
-        userid = row[0][0] + 1
-        row = session.execute("INSERT INTO unisupport.users (userid, accounttype, email, password, username) VALUES (%s, %s, %s, %s, %s);",[userid, 'User', email, password, username])
+
+        try:
+            row = session.execute('SELECT MAX(userid) AS max FROM unisupport.users')
+            userid = row[0][0] + 1
+        except:
+            userid = 0
+
+        row = session.execute("INSERT INTO unisupport.users (userid, accounttype, email, password, username) VALUES (%s, %s, %s, %s, %s);", [userid, 'User', email, password, username])
         response={"bool": True, "user": username, "message": "Succesfully signed up as "+username}
     else:
         response = {"bool": False, "user": "", "message": "Unsuccesfully signed up."}
